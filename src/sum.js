2630e17532ce43f98af2ca7839706f6b7d123835
tawwy.tran <tawwy.tran@gmail.com>
2020-11-04 18:57:51 +0100

    fullfills task c

--- src/sum.js
+++ src/sum.js
@@ -1,17 +1,26 @@
 
 function stringCalc(numbers) {
-
+	
 	var eingabe = String(numbers);
-	var stSplit = eingabe.split(',');
+	var stSplit = eingabe.split(/,|\n/);
+
+	//Leere String-Elemente im Array löschen und herausfiltern
+	
+	stSplit = stSplit.filter(function(x) 
+		{
+			return x != null && x != "";
+		});
+
+	console.log(stSplit);
 
 	if (stSplit.length > 1) 
 		{	
 			var addition = 0;
 
 			for(var i = 0; i <= stSplit.length-1; i++)
-			{
-				addition = parseInt(addition) + parseInt(stSplit[i]);
-			}
+				{
+					addition = parseInt(addition) + parseInt(stSplit[i]);
+				}
 		
 			return addition;
 		}
@@ -20,6 +29,7 @@ function stringCalc(numbers) {
 			var addition = parseInt(stSplit[0]);
 			return addition;
 		}
+
 }
 
 const sum = stringCalc();
--- src/sum.test.js
+++ src/sum.test.js
@@ -1,25 +1,25 @@
 const { stringCalc } = require('./sum');
 
 describe('stringCalc', () => {
-	it('should add 1, 2 and 3 to be 6', () => {
+	it('should add 1, 2, 3 to be 6', () => {
 		expect(stringCalc("1,2,3")).toBe(6)
 	})
 } )
 
 describe('stringCalc', () => {
-	it('should add 4, 5, 6 and 7 to be 22', () => {
+	it('should add 4, 5, 6, 7 to be 22', () => {
 		expect(stringCalc("4,5,6,7")).toBe(22)
 	})
 } )
 
 describe('stringCalc', () => {
-	it('should add 1, 2, 3, 4, 5, 6, 7 and 8 to be 36', () => {
+	it('should add 1, 2, 3, 4, 5, 6, 7, 8 to be 36', () => {
 		expect(stringCalc("1,2,3,4,5,6,7,8")).toBe(36)
 	})
 } )
 
 describe('stringCalc', () => {
-	it('should add 10 and 21 to be 31', () => {
+	it('should add 10, 21 to be 31', () => {
 		expect(stringCalc("10,21")).toBe(31)
 	})
 } )
@@ -38,7 +38,54 @@ describe('stringCalc', () => {
 
 
 describe('stringCalc', () => {
-	it('should add ,1,2,3 to be NaN', () => {
-		expect(stringCalc(",1,2,3")).toBe(NaN)
+	it('should add ,1,2,3 to be 6', () => {
+		expect(stringCalc(",1,2,3")).toBe(6)
+	})
+} )
+
+//newline
+
+describe('stringCalc', () => {
+	it('should add 1 \n 2, 3 to be 6', () => {
+		expect(stringCalc("1\n2,3")).toBe(6)
+	})
+} )
+
+
+describe('stringCalc', () => {
+	it('should add 1 \n 2 \n 3 to be 6', () => {
+		expect(stringCalc("1\n2\n3")).toBe(6)
+	})
+} )
+
+
+describe('stringCalc', () => {
+	it('should add 1 ,\n 2 \n 3 to be 6', () => {
+		expect(stringCalc("1,\n2\n3")).toBe(6)
+	})
+} )
+
+describe('stringCalc', () => {
+	it('should add 1 ,\n 2, 3 to be 6', () => {
+		expect(stringCalc("1,\n2,3")).toBe(6)
+	})
+} )
+
+
+describe('stringCalc', () => {
+	it('should add 1 ,\n 2 ,\n 3 to be 6', () => {
+		expect(stringCalc("1,\n2,\n3")).toBe(6)
+	})
+} )
+
+describe('stringCalc', () => {
+	it('should add 1 ,\n 2 ,\n 3 ,4 to be 10', () => {
+		expect(stringCalc("1,\n2,\n3,4")).toBe(10)
+	})
+} )
+
+describe('stringCalc', () => {
+	it('should add 1 ,\n 2 ,\n 3 ,4\n5 to be 15', () => {
+		expect(stringCalc("1,\n2,\n3,4\n5")).toBe(15)
 	})
 } )
